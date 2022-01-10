93d2908dcc5a3e8ae556125536ffc6e0629fe1b0
Eli <ehanover21@gmail.com>
2022-01-10 18:11:42 -0500

    first hack, pull from snapshot

---
+++ config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    $KRAUSE: "0x9f6f91078a5072a8b54695dafa2374ab3ccd603b",
+    balanceOfAbi: ["function balanceOf(address owner) view returns (uint256)"],
+    CROWDFUND: "0xc4e0f3ec24972c75df7c716922096f4270b7bb4e",
+    WILLCALL: "0x6F1183aB121e9F05CA4916237012Ed37B6f20583",
+    UPPER_LEVEL_ID: 61,
+    CLUB_LEVEL_EDITION_ID: 60,
+    COURTSIDE_EDITION_ID: 59
+}
