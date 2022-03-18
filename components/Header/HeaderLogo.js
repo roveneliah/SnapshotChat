2d7f2ba008da6d9512bab4ab1c32c63ae6e8d726
Eli <ehanover21@gmail.com>
2022-03-18 15:26:28 -0400

    Add link back to home in Header Logo.

--- components/Header/HeaderLogo.js
+++ components/Header/HeaderLogo.js
@@ -2,16 +2,18 @@ import kh_holo from "../../public/kh_holo.png";
 import Image from "next/image";
 
 export const HeaderLogo = () => (
-  <div className="flex space-x-2">
-    <Image
-      src={kh_holo}
-      className="flex"
-      width={40}
-      height={40}
-      alt="KH_LOGO"
-    />
-    <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
-      The Krause House
-    </span>
-  </div>
+  <a href="/">
+    <div className="flex space-x-2 hover:opacity-75">
+      <Image
+        src={kh_holo}
+        className="flex"
+        width={40}
+        height={40}
+        alt="KH_LOGO"
+      />
+      <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
+        The Krause House
+      </span>
+    </div>
+  </a>
 );
