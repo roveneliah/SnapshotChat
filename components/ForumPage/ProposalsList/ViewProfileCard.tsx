0a499b0af7d5e86dc5d375632d271e587a3ce512
Eli <ehanover21@gmail.com>
2022-03-18 17:22:32 -0400

    Minor bug fix

--- components/ForumPage/ProposalsList/ViewProfileCard.tsx
+++ components/ForumPage/ProposalsList/ViewProfileCard.tsx
@@ -11,7 +11,7 @@ export const ViewProfileCard = () => {
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Follow other Jerry's to curate their opinions on proposals.
+            Follow other Jerrys to curate their opinions on proposals.
           </p>
         </div>
         <Button
--- components/Header/HeaderLogo.js
+++ components/Header/HeaderLogo.js
@@ -1,9 +1,10 @@
 import kh_holo from "../../public/kh_holo.png";
 import kh_logo from "../../public/kh_logo2.png";
 import Image from "next/image";
+import Link from "next/link";
 
 export const HeaderLogo = () => (
-  <a href="/">
+  <Link href="/">
     <Image
       src={kh_logo}
       width={150}
@@ -11,5 +12,5 @@ export const HeaderLogo = () => (
       alt="KH_LOGO"
       className="hover:opacity-75"
     />
-  </a>
+  </Link>
 );
