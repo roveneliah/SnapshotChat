cec09051cb378cae7158280bdbd510e2d69c1b83
Eli <ehanover21@gmail.com>
2022-03-18 15:23:46 -0400

    Modularize config
    This commit is an initial attempt to abstract config such that we can easily swap out governance tokens, logo's, dbs, such that other DAOs can use this.

---
+++ config/navItems.ts
@@ -0,0 +1,42 @@
+export const navItems = [
+  {
+    title: "Governance",
+    href: "/",
+  },
+  {
+    title: "Profile",
+    href: "/profile",
+  },
+  //   {
+  //     title: "Delegates",
+  //     href: "/delegates",
+  //   },
+  // {
+  //   title: "Petitions",
+  //   href: "/petitions",
+  // },
+  // {
+  //   title: "Roster",
+  //   href: "https://roster.krausehouse.club/",
+  // },
+  // {
+  //   title: "Analytics",
+  //   href: "https://analytics.krausehouse.club/",
+  // },
+  // {
+  //   title: "Bounties",
+  //   href: "https://app.dework.xyz/o/krause-house-0M48tOstbs6BKTdlGNrWNU",
+  // },
+  // {
+  //   title: "Contribute",
+  //   href: "/pods",
+  // },
+  // {
+  //   title: "Merch",
+  //   href: "/merch",
+  // },
+  // {
+  //   title: "Collection",
+  //   href: "/collection",
+  // },
+];
