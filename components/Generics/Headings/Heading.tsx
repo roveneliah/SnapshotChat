0e3c1eac98420bd32e32666c58aeefb5dfcc2e8c
Eli <ehanover21@gmail.com>
2022-05-11 10:19:09 +0800

    ts fix

--- components/Generics/Headings/Heading.tsx
+++ components/Generics/Headings/Heading.tsx
@@ -7,7 +7,13 @@ const styles = {
   "2xl": "mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white",
 };
 
-export const Heading = ({ title, size, className }) => {
+interface Props {
+  title?: String;
+  size?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
+  className?: String;
+}
+
+export const Heading = ({ title = "", size = "lg", className = "" }: Props) => {
   // const className = styles[size];
   return <h5 className={`${styles[size]} ${className}`}>{title}</h5>;
 };
